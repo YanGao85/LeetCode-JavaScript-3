5da635df259d20c0b3fecaac631a1c74cd78c5f6
xTobu <junxiang.oriint@gmail.com>
2018-05-29 17:01:25 +0800

    更新

--- #1/1-Two Sum.js
+++ #1/1-Two Sum.js
@@ -3,12 +3,12 @@ var twoSum = function(nums, target) {
 	var val;
 	for (var index = 0; index < nums.length; index++) {
 		val = nums[index];
-		//將 target - 當前的val, 如果 temp[target - val] >= 0 , 則代表在temp找得到key 
+		//將 target - 當前的val, 如果 temp[target - val] >= 0 , 則代表在temp找得到key
 		//且值大於或不等於0 則代表找到了, 即可return 陣列
 		if (temp[target - val] >= 0) {
 			//console.log(temp[target-val],index)
 			return [temp[target - val], index];
-		} 
+		}
 		// 找不到的話, 則將目前的 val, 當作temp的一個 key 去儲存起來,
 		// 其 key 的 val 則為目前的index
 		else {
@@ -16,4 +16,3 @@ var twoSum = function(nums, target) {
 		}
 	}
 };
-
