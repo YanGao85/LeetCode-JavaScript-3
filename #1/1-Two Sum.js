aee464ea67ba45125a851834518e533a5eb442b4
xTobu <junxiang.oriint@gmail.com>
2018-05-29 16:22:10 +0800

    更新註解

--- #1/1-Two Sum.js
+++ #1/1-Two Sum.js
@@ -3,17 +3,17 @@ var twoSum = function(nums, target) {
 	var val;
 	for (var index = 0; index < nums.length; index++) {
 		val = nums[index];
-		//將target(9) - 當前的val,如果在temp找得到key代表有值 且值大於或不等於0
-		//則代表找到了, 即可return 陣列
+		//將 target - 當前的val, 如果 temp[target - val] >= 0 , 則代表在temp找得到key 
+		//且值大於或不等於0 則代表找到了, 即可return 陣列
 		if (temp[target - val] >= 0) {
 			//console.log(temp[target-val],index)
 			return [temp[target - val], index];
-		} else {
-			//將陣列巡迴的值,存做temp的key值, value存為陣列的第幾個
+		} 
+		// 找不到的話, 則將目前的 val, 當作temp的一個 key 去儲存起來,
+		// 其 key 的 val 則為目前的index
+		else {
 			temp[val] = index;
 		}
 	}
 };
 
-
-//https://leetcode.com/problems/two-sum/description/
