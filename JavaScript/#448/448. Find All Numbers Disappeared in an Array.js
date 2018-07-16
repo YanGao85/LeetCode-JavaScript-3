a809502fbcd383fc91c69069216de120d470758c
Junxiang <junxiang.oriint@gmail.com>
2018-07-16 12:35:38 +0800

    wip

--- JavaScript/#448/448. Find All Numbers Disappeared in an Array.js
+++ JavaScript/#448/448. Find All Numbers Disappeared in an Array.js
@@ -2,4 +2,18 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findDisappearedNumbers = function(nums) {};
+
+/**
+ * 第一種寫法
+ * nums.indexOf(i) 的能太差,
+ * 在leetcode上只落在倒數的答案,
+ * 母湯, 趕緊爬文
+ */
+
+var findDisappearedNumbers = function(nums) {
+	var res = [];
+	for (var i = 1; i < nums.length + 1; i++) {
+		if (nums.indexOf(i) === -1) res.push(i);
+	}
+	return res;
+};
