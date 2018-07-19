716415873efcab5f390915a472b70ad530a62600
Junxiang <junxiang.oriint@gmail.com>
2018-07-19 16:26:11 +0800

    新增 #22

---
+++ JavaScript/#22/22. Generate Parentheses.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+
+var generateParenthesis = function(n) {
+	const arrResult = [];
+	let countRecurse = 0;
+	function recurse(current = '', left = 0, right = 0) {
+		countRecurse++
+		console.log('countRecurse: ',countRecurse,'｜','current :', current, '｜', left, right);
+		// 等長度組到了 n*2 (括號長度為二) 的時候, 即可推到 arrResult
+		if (current.length === 2 * n) {
+			arrResult.push(current);
+			console.log('== push and return ==');
+			return;
+		}
+
+		if (left < n) recurse(current + '(', left + 1, right);
+		if (right < left) recurse(current + ')', left, right + 1);
+		return arrResult
+	}
+	// 執行遞迴
+	// 回傳結果
+	return recurse();
+};
+console.log(generateParenthesis(3));
---
+++ JavaScript/#22/README.md
@@ -0,0 +1,29 @@
+# 22. Generate Parentheses 產生括號
+
+## Origin
+
+### Question
+
+Given _n_ pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+### Example
+
+```
+Input: n = 3
+Output: 
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+```
+
+---
+
+## 翻譯
+
+### 問題
+
+給定 *n* 對的括號, 編寫一個函數來產生所有格式正確的括號的組合.  
